4b272bd1b366e9ff567d812deb15f8c117d4eaa2
Aryan Padmakar Shirsat <aryanshirsat357@gmail.com>
2023-10-24 19:13:33 +0530

    updated addinvestment.jsx

--- client/src/Components/Addinvestment.jsx
+++ client/src/Components/Addinvestment.jsx
@@ -3,7 +3,8 @@ import {React, useState} from "react";
 const Addexpense = ({ showModal, handleCloseModal }) => {
     const [formData, setFormData] = useState({
         category: '',
-        amount: ''
+        amount: '',
+        cashFlowAmount: '',
       });
     
       const handleInputChange = (e) => {
@@ -45,6 +46,37 @@ const Addexpense = ({ showModal, handleCloseModal }) => {
             <option value="Entertainment">Mutual funds</option>
             {/* Add more categories as needed */}
           </select>
+        </div>
+        <br />
+        <div className="form-group">
+          <label htmlFor="category">Does this investment generate cash flow:</label>
+          <select
+            id="category"
+            name="category"
+            value={formData.category}
+            onChange={handleInputChange}
+            style={{backgroundColor: 'rgb(32,32,32)', border: 'none'}}
+            required
+          >
+            <option value="">Select Category</option>
+            <option value="Food">YES</option>
+            <option value="Transportation">NO</option>
+            {/* Add more categories as needed */}
+          </select>
+          {formData.category === 'Food' && (
+        <div className="form-group">
+          <label htmlFor="cashFlowAmount">Cash Flow Amount(per month):</label>
+          <input
+            type="text"
+            id="cashFlowAmount"
+            name="cashFlowAmount"
+            value={formData.cashFlowAmount}
+            onChange={handleInputChange}
+            style={{ backgroundColor: 'rgb(32,32,32)', border: 'none' }}
+            required
+          />
+        </div>
+      )}
         </div>
         <br />
         <div className="form-group">
